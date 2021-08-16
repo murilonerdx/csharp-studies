b62c615695fa0a24c1ecacac7bf771517422d906
murilonerdx <mu-silva@outlook.com>
2021-08-16 14:50:17 -0300

    Criando classe

---
+++ Triangulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
