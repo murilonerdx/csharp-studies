2f628cd6563cfb1465cc467eb7c48d3c95c05e4d
murilonerdx <mu-silva@outlook.com>
2021-08-16 14:16:58 -0300

    Tipos de variaveis

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 214748368L;
+            char genero = 'F';
+            double n5 = 4.5;
+            float n6 = 4.5f;
+            string nome = "Maria Green";
+            object obj1 = "Murilo Pereira";
+            object obj2 = 4.5f;
+            string nome2 = "Murilo Pereira da Silva";
+           
+
+
+            Console.WriteLine(completo);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(genero);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(nome2);
+         
+        }
+    }
+}
